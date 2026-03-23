e56c995a8b81321d8936014f5f3c010009cc9948
aldrinalladin-ui <aldrin.alladin@holychild.edu.ph>
2026-03-23 09:29:15 +0800

    create.index aldrin

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'connect.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Site</title>
+</head>
+<body>
+
+<h1>Welcome to my website</h1>
+
+<?php
+echo "<p>This is my index.php file.</p>";
+?>
+
+</body>
+</html>
